a783219e4e68df3e8e9c0ec3bedb1d6a2b82b150
manuelsilva01 <manueee200214@gmail.com>
2025-09-07 20:45:13 -0500

    Delete Propuesta LandingPage y Propuesta Gráfica_MS directory

--- Propuesta LandingPage y Propuesta Gráfica_MS/scripts/script.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("miFormulario");
-const ruta = document.getElementById("ruta");
-const espacio = document.getElementById("espacio");
-const tiempo = document.getElementById("tiempo");
-const recomentaciones = document.getElementById("recomentaciones");
-
-form.addEventListener("submit", () => {
-    alert("¡La evaluación será enviada!")
-})
